7f65f56af0161804e1bb24bf4e1f9a0d60097cac
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-01-20 16:36:17 -0600

    fix: product insert

---
+++ initdb/Products_Insert.sql
@@ -0,0 +1,130 @@
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        'ac80944a-f9c2-49cf-a67f-29336ddb396b', 'Product 1', 'Description for Product 1', 343.31, 922,
+        '2024-10-16 01:56:26',
+        NULL,
+        '2024-11-23 01:56:26',
+        'SYSTEM',
+        NULL,
+        'SYSTEM',
+        FALSE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        'ff88b7be-787b-4db0-8eb8-31bd2a45a3db', 'Product 2', 'Description for Product 2', 345.41, 509,
+        '2024-01-28 12:23:47',
+        '2024-01-30 12:23:47',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        'be16d2c6-1603-401a-81bb-deeca464da2a', 'Product 3', 'Description for Product 3', 310.63, 2,
+        '2024-11-01 09:23:33',
+        NULL,
+        '2024-11-16 09:23:33',
+        'SYSTEM',
+        NULL,
+        'SYSTEM',
+        FALSE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '3751ad0d-6965-4dcb-992f-d28706eb9fc1', 'Product 4', 'Description for Product 4', 77.24, 248,
+        '2024-01-29 00:04:13',
+        NULL,
+        '2024-02-17 00:04:13',
+        'SYSTEM',
+        NULL,
+        'SYSTEM',
+        FALSE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        'f219ba3d-387d-450b-8bf9-57741cb5cf2f', 'Product 5', 'Description for Product 5', 72.39, 980,
+        '2024-11-30 22:23:08',
+        '2024-12-21 22:23:08',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '391ff5fc-b29d-415f-9beb-2f2c7eeb1c91', 'Product 6', 'Description for Product 6', 251.93, 131,
+        '2024-04-19 13:19:05',
+        '2024-04-28 13:19:05',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '102bf970-f304-420d-afdb-b1ffb2198089', 'Product 7', 'Description for Product 7', 344.38, 701,
+        '2024-09-17 23:05:45',
+        '2024-10-03 23:05:45',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '31d369ba-dd35-41bd-9f03-fb85e0fdad8e', 'Product 8', 'Description for Product 8', 219.31, 127,
+        '2024-12-15 04:16:52',
+        NULL,
+        '2025-01-16 04:16:52',
+        'SYSTEM',
+        NULL,
+        'SYSTEM',
+        FALSE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '07ff3942-9064-4be0-ba1c-7c85b45aa23d', 'Product 9', 'Description for Product 9', 453.14, 220,
+        '2024-05-20 11:27:31',
+        '2024-06-19 11:27:31',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
+INSERT INTO public."Products" (
+        "Id", "Name", "Description", "Price", "Stock", "CreatedAt", "UpdatedAt", "DeletedAt",
+        "CreatedBy", "UpdatedBy", "DeletedBy", "IsActive", "IsDeleted"
+    ) VALUES (
+        '4ade2bdb-a908-436f-a476-63fa96ab9292', 'Product 10', 'Description for Product 10', 367.53, 755,
+        '2024-05-06 23:49:28',
+        '2024-06-03 23:49:28',
+        NULL,
+        'SYSTEM',
+        'SYSTEM',
+        NULL,
+        TRUE, FALSE
+    );
